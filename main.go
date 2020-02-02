01950b4f6fd0a6404568275df4319264351e89b9
kraxarn <me@kraxarn.com>
2020-02-03 00:38:49 +0100

    Move listing functions

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -33,39 +31,6 @@ func FormatFileSize(size int64) string {
 	}
 	// b
 	return fmt.Sprintf("%d", size)
-}
-
-func IsFile(path string) bool {
-	info, err := os.Stat(filepath.Join("files", path))
-	return err != nil || !info.IsDir()
-}
-
-func List(path string) []FileInfo {
-	fileInfo := make([]FileInfo, 0)
-	files, _ := ioutil.ReadDir(filepath.Join("files", path))
-	for _, file := range files {
-		fileInfo = append(fileInfo, FileInfo{
-			Size:         file.Size(),
-			DateModified: file.ModTime().Format("2006-01-02 15:04"),
-			Name:         file.Name(),
-			IsDirectory:  file.IsDir(),
-		})
-	}
-	return fileInfo
-}
-
-func HandleList(context *gin.Context) {
-	path := filepath.Join(
-		context.FullPath()[:len(context.FullPath())-5],
-		context.Param("file"))
-	if IsFile(path) {
-		context.File(filepath.Join("files", path))
-	} else {
-		context.HTML(http.StatusOK, "ls.html", gin.H{
-			"path": path,
-			"dir": List(path),
-		})
-	}
 }
 
 func main() {
