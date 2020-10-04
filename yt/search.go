cf4bc8792d0ad131b0c124c0d06f9e5f40e5a9b6
kraxarn <me@kraxarn.com>
2020-10-04 13:15:22 +0200

    Initial web scraping search

--- yt/search.go
+++ yt/search.go
@@ -3,7 +3,10 @@ package yt
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"reflect"
+	"strings"
 )
 
 type SearchResult struct {
@@ -24,28 +27,77 @@ type SearchResponse struct {
 	Description     string
 }
 
+// TODO: TESTING ONLY
+func Search(query string) ([]SearchResult, error) {
+	return search(query)
+}
+
+func between(s, start, end string) (string, error) {
+	startIndex := strings.Index(s, start) + len(start)
+	if startIndex < 0 {
+		return "", fmt.Errorf("start not found in string")
+	}
+
+	endIndex := strings.Index(s[startIndex:], end) + startIndex
+	if endIndex < 0 {
+		return "", fmt.Errorf("end not found in string")
+	}
+
+	return s[startIndex:endIndex], nil
+}
+
+func dig(data map[string]interface{}, parameters []string) (map[string]interface{}, error) {
+	endpoint := data
+
+	for i, param := range parameters {
+		/*if arr, ok := endpoint[param].([]interface{}); ok {
+			endpoint = arr[0].(map[string]interface{})
+		}*/
+		if newEndpoint, ok := endpoint[param].(map[string]interface{}); ok {
+			endpoint = newEndpoint
+		} else {
+			return endpoint, fmt.Errorf("property %s (index %d) does not exist in map (%v)", param, i, reflect.ValueOf(endpoint).MapKeys())
+		}
+	}
+
+	return endpoint, nil
+}
+
 func search(query string) ([]SearchResult, error) {
-	result, err := http.Get(fmt.Sprintf("%s/api/v1/search?q=%s", invidiousPath, query))
+	result, err := http.Get(fmt.Sprintf("https://www.youtube.com/results?search_query=%s", query))
 	if err != nil {
 		return nil, err
 	}
-	defer result.Body.Close()
+	defer func() {
+		_ = result.Body.Close()
+	}()
 
-	var responses []SearchResponse
-	err = json.NewDecoder(result.Body).Decode(&responses)
+	bodyData, err := ioutil.ReadAll(result.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	var results []SearchResult
-	for _, response := range responses {
-		results = append(results, SearchResult{
-			Description: response.Description,
-			Id:          response.VideoId,
-			Thumbnail:   response.VideoThumbnails[0].Url,
-			Title:       response.Title,
-		})
+	body := string(bodyData)
+	data, err := between(body, "window[\"ytInitialData\"] = ", ";")
+	if err != nil {
+		return nil, err
 	}
 
-	return results, nil
+	var response map[string]interface{}
+	err = json.Unmarshal([]byte(data), &response)
+	if err != nil {
+		return nil, err
+	}
+
+	digResponse, err := dig(response, []string{
+		"contents", "twoColumnSearchResultsRenderer", "primaryContents", "sectionListRenderer",
+		"contents", "itemSectionRenderer",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	_ = digResponse["contents"].([]interface{})
+
+	return nil, nil
 }
