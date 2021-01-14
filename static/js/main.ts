7fa6ae009ccbe7765cdce3a8aebc4aa8de14c81d
kraxarn <me@kraxarn.com>
2021-01-14 22:57:53 +0100

    Remove TypeScript for now

--- static/js/main.ts
+++
@@ -1,3 +0,0 @@
-const getById = (elementId: string): HTMLElement => document.getElementById(elementId)
-
-getById("background").style.backgroundImage = `url("/img/bg/${Math.floor(new Date().getHours() / 2)}.webp")`
