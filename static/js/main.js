cc694ca53bebcb894f6a66645217da14432d790e
kraxarn <me@kraxarn.com>
2021-01-23 16:31:46 +0100

    Make sure background exists

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,9 @@
 const getById = (elementId) => document.getElementById(elementId)
 
-getById("background").style.backgroundImage = `url("/img/bg/${Math.floor(new Date().getHours() / 2)}.webp")`
+const setBackground = () => {
+	const background = getById("background")
+	if (background) {
+		background.style.backgroundImage = `url("/img/bg/${Math.floor(new Date().getHours() / 2)}.webp")`
+	}
+}
+setBackground()
