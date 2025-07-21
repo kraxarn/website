4e4785fcf14ae4d4d5fe0580b3f98474e534f333
kraxarn <kraxie@protonmail.com>
2025-07-21 15:06:45 +0200

    Fix missing files for highlight

---
+++ static/js/highlight.js
@@ -0,0 +1,7 @@
+(() => {
+    const body = document.querySelector("body");
+    document.addEventListener("mousemove", (event) => {
+        body.style.backgroundPosition = `left ${event.clientX}px top ${event.clientY}px`;
+    });
+})();
+//# sourceMappingURL=highlight.js.map
