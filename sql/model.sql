a4dcf53f89a1c39b291ccaaec44af74221a6ee05
kraxarn <kraxie@protonmail.com>
2025-07-22 02:31:03 +0200

    Initial db model

---
+++ sql/model.sql
@@ -0,0 +1,16 @@
+create table users
+(
+    id       serial  not null primary key,
+    username text    not null,
+    password bytea   not null,
+    flags    integer not null
+);
+
+create table texts
+(
+    id        serial      not null primary key,
+    key       text        not null,
+    value     text        not null,
+    editor    serial      not null references users (id),
+    timestamp timestamptz not null
+);
