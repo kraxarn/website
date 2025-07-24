d6c65b69f3e5889a625e7fd1c6c609e0525cf48c
kraxarn <kraxie@protonmail.com>
2025-07-24 16:11:24 +0200

    Initial support for load/save

--- group/admin.go
+++ group/admin.go
@@ -5,12 +5,28 @@ import (
 	"net/http"
 )
 
+type editorContent struct {
+	Key   string `form:"key"`
+	Value string `form:"value"`
+	Type  string `form:"type"`
+}
+
 func RegisterAdmin(app *echo.Echo) {
 	group := app.Group("/admin")
 
 	group.GET("/editor", editor)
+	group.POST("/editor", editorData)
 }
 
 func editor(ctx echo.Context) error {
+	return ctx.Render(http.StatusOK, "editor.gohtml", nil)
+}
+
+func editorData(ctx echo.Context) error {
+	var content editorContent
+	if err := ctx.Bind(&content); err != nil {
+		return err
+	}
+
 	return ctx.Render(http.StatusOK, "editor.gohtml", nil)
 }
