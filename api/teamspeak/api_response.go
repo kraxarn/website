ca45435cd37203a81d5c83bd4c9df96602a05ef1
kraxarn <kraxie@protonmail.com>
2025-08-16 18:09:50 +0200

    Add status endpoint

--- api/teamspeak/api_response.go
+++ api/teamspeak/api_response.go
@@ -1,5 +1,7 @@
 package teamspeak
 
+import "errors"
+
 type ApiStatus struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
@@ -8,4 +10,12 @@ type ApiStatus struct {
 type ApiResponse[T interface{}] struct {
 	Body   T         `json:"body"`
 	Status ApiStatus `json:"status"`
+}
+
+func StatusError[T interface{}](resp ApiResponse[T]) error {
+	if resp.Status.Code == 0 {
+		return nil
+	}
+
+	return errors.New(resp.Status.Message)
 }
--- group/servers.go
+++ group/servers.go
@@ -1,14 +1,19 @@
 package group
 
 import (
+	"fmt"
+	"github.com/kraxarn/website/api/teamspeak"
 	"github.com/kraxarn/website/helper"
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func RegisterServers(app *echo.Echo) {
 	group := app.Group("/servers")
 
 	group.GET("", servers)
+
+	group.GET("/teamspeak/status", teamSpeakStatus)
 }
 
 func servers(ctx echo.Context) error {
@@ -16,4 +21,27 @@ func servers(ctx echo.Context) error {
 		"styles":  []string{"servers"},
 		"scripts": []string{"servers"},
 	})
+}
+
+func teamSpeakStatus(ctx echo.Context) error {
+	api, err := teamspeak.NewApi()
+	if err != nil {
+		return err
+	}
+
+	var resp teamspeak.ApiResponse[teamspeak.HostInfo]
+	resp, err = api.HostInfo()
+	if err != nil {
+		return err
+	}
+
+	if err = teamspeak.StatusError(resp); err != nil {
+		return err
+	}
+
+	str := fmt.Sprintf("%s/%s",
+		resp.Body.VirtualServersTotalClientsOnline,
+		resp.Body.VirtualServersTotalMaxClients,
+	)
+	return ctx.String(http.StatusOK, str)
 }
