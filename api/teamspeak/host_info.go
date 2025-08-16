459b84300cd4af7f886921ef58a5cc0a140e5de6
kraxarn <kraxie@protonmail.com>
2025-08-16 17:51:49 +0200

    Add host info method

---
+++ api/teamspeak/host_info.go
@@ -0,0 +1,16 @@
+package teamspeak
+
+type HostInfo struct {
+	HostTimestampUtc                  string `json:"host_timestamp_utc"`
+	InstanceUptime                    string `json:"instance_uptime"`
+	VirtualServersRunningTotal        string `json:"virtualservers_running_total"`
+	VirtualServersTotalChannelsOnline string `json:"virtualservers_total_channels_online"`
+	VirtualServersTotalClientsOnline  string `json:"virtualservers_total_clients_online"`
+	VirtualServersTotalMaxClients     string `json:"virtualservers_total_maxclients"`
+}
+
+func (a Api) HostInfo() (ApiResponse[HostInfo], error) {
+	var response ApiResponse[HostInfo]
+	err := a.get("/hostinfo", &response)
+	return response, err
+}
